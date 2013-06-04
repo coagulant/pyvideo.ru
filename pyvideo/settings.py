17dad94db77966d1fe2eb1c563f53de882bf7a3b
coagulant <baryshev@gmail.com>
2013-06-04 09:57:17 +0400

    Static files root changed to be inside project dir

--- pyvideo/settings.py
+++ pyvideo/settings.py
@@ -1,7 +1,10 @@
 # Django settings for pyvideo project.
+import os
 from richard.settings import *
 
-ALLOWED_HOSTS = ['pyvideo.ru', 'pyvideoru.herokuapp.com']
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+ALLOWED_HOSTS = ['pyvideo.ru', 'pyvideoru.herokuapp.com', '127.0.0.1']
 TIME_ZONE = 'Europe/Moscow'
 LANGUAGE_CODE = 'ru'
 SECRET_KEY = 'this_is_not_production_so_who_cares'
@@ -14,6 +17,8 @@ TEMPLATE_DIRS = (
     os.path.join(ROOT, 'templates'),
 )
 
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+
 INSTALLED_APPS = INSTALLED_APPS + (
     'raven.contrib.django.raven_compat',
 )
@@ -24,4 +29,3 @@ DATABASES = {
         'NAME': 'database.db',
     }
 }
-
