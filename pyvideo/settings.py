deb7821f008eb6aa05b05fa330533b659bd0e79e
coagulant <baryshev@gmail.com>
2013-06-08 13:29:48 +0400

    Django-suit

--- pyvideo/settings.py
+++ pyvideo/settings.py
@@ -10,24 +10,29 @@ LANGUAGE_CODE = 'ru'
 SECRET_KEY = 'this_is_not_production_so_who_cares'
 
 ROOT_URLCONF = 'pyvideo.urls'
-
 WSGI_APPLICATION = 'pyvideo.wsgi.application'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_PATH, 'templates'),
 )
-
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request',
+)
+JINGO_EXCLUDE_APPS += (
+    'suit',
+)
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
-INSTALLED_APPS += (
+INSTALLED_APPS = (
     'raven.contrib.django.raven_compat',
-)
+    'suit',
+) + INSTALLED_APPS
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'database.db',
-        }
     }
+}
 
 API = True
