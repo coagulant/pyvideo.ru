db423c15148719cce1febcc6058484ec425d5d9c
coagulant <baryshev@gmail.com>
2013-11-28 23:38:15 +0400

    Automatic links in markdown

--- sergey/helpers.py
+++ sergey/helpers.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from django.conf import settings
 from jingo import register
+from jinja2 import Markup
+import markdown
 
 
 @register.function
@@ -11,3 +13,15 @@ def header(s=None):
     if len(s) > 80:
         s = s[:80] + u'...'
     return u'%s - %s' % (s, settings.SITE_TITLE)
+
+
+@register.filter
+def md(text):
+    """Filter that converts Markdown text -> HTML."""
+    return Markup(
+        markdown.markdown(
+            text,
+            output_format='html5',
+            extensions=['sergey.urlize'],
+            safe_mode='replace',
+            html_replacement_text='[HTML REMOVED]'))
