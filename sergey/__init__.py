eb07188708c69674022cbb4153cc3733927af92a
Sergei K <kh.sergei@gmail.com>
2014-07-14 15:01:34 +0400

    Use advanced slugify that is able to handle unicode

--- sergey/__init__.py
+++ sergey/__init__.py
@@ -1,1 +1,7 @@
 # coding: utf-8
+
+from richard.videos import utils
+from slugify import slugify
+
+# monkey patch richard.videos.utils.slugify so it handles all unicode characters
+utils.slugify = slugify
