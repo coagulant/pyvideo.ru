099dacf4be3bda9139a74eda7a30dc43d87848e9
Sergei Khoroshilov <kh.sergei@gmail.com>
2014-07-15 23:39:11 +0400

    Switch to the django template engine

--- sergey/templatetags/sergey.py
+++ sergey/templatetags/sergey.py
@@ -1,27 +1,34 @@
 # coding: utf-8
+from __future__ import unicode_literals
+
 from django.conf import settings
-from jingo import register
-from jinja2 import Markup
+from django import template
+from django.utils.html import mark_safe
 import markdown
 
+register = template.Library()
 
-@register.function
-def header(s=None):
+
+@register.simple_tag
+def header(*args):
     """Function that generates the page title."""
-    if s is None:
+    if not args:
         return settings.SITE_TITLE
+    s = ''.join(args)
     if len(s) > 80:
-        s = s[:80] + u'...'
-    return u'%s - %s' % (s, settings.SITE_TITLE)
+        s = s[:80] + '...'
+    return '%s - %s' % (s, settings.SITE_TITLE)
 
 
 @register.filter
 def md(text):
     """Filter that converts Markdown text -> HTML."""
-    return Markup(
+    return mark_safe(
         markdown.markdown(
             text,
             output_format='html5',
             extensions=['sergey.urlize', 'nl2br'],
             safe_mode='replace',
-            html_replacement_text='[HTML REMOVED]'))
+            html_replacement_text='[HTML REMOVED]'
+        )
+    )
