28f05ebd037d2fb6a25deff6f12c8c704d92c7e7
Ilya Baryshev <baryshev@gmail.com>
2014-07-20 12:40:32 +0400

    Improve video admin

--- sergey/admin.py
+++ sergey/admin.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 from django.contrib import admin
-from richard.videos.admin import VideoAdmin
+from richard.videos.admin import VideoAdmin, SpeakerAdmin
 from richard.videos.models import Speaker, Video
 
 
-class SpeakerAdmin(admin.ModelAdmin):
+class PySpeakerAdmin(SpeakerAdmin):
     list_display_links = ('link',)
     list_display = ('link', 'name', 'slug')
     list_editable = ('name', 'slug',)
@@ -16,13 +16,20 @@ class SpeakerAdmin(admin.ModelAdmin):
 
 
 admin.site.unregister(Speaker)
-admin.site.register(Speaker, SpeakerAdmin)
+admin.site.register(Speaker, PySpeakerAdmin)
 
 
 class PyVideoAdmin(VideoAdmin):
-    list_display = ('title', 'category', 'speakers_string', 'whiteboard', 'state')
+    list_display = ('title', 'category', 'tags_string', 'speakers_string', 'whiteboard', 'state')
+    list_editable = ('whiteboard',)
     search_fields = ('title', 'slug')
     ordering = ('-updated',)
+    fields = ('title', 'slug', 'tags', 'description', 'category', 'speakers',
+              'language', 'source_url', 'embed', 'thumbnail_url', 'duration',
+              'recorded', 'state')
+
+    def tags_string(self, obj):
+        return u', '.join(obj.tags.values_list('tag', flat=True))
 
     def speakers_string(self, obj):
         return u', '.join(obj.speakers.values_list('name', flat=True))
