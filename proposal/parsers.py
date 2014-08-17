9af607785f916640b090cae56cc3af40630467e1
Sergei Khoroshilov <kh.sergei@gmail.com>
2014-08-17 17:49:02 +0400

    Refactor proposal parser

--- proposal/parsers.py
+++ proposal/parsers.py
@@ -4,123 +4,173 @@ import re
 from django.core.serializers import SerializerDoesNotExist
 from django.conf import settings
 from rest_framework.settings import api_settings
+from richard.videos.models import Category
 
 from .serializers import CategorySerializer, VideoSerializer
 from .utils import force_path
-from .exceptions import TemplateError, ObjectError
+from .exceptions import ProposalError, TemplateError, ObjectError
 
 
-def deserialize(format, stream):
-    """
-    Attempt to read and deserialize data from ``stream``.
-    Return a deserialized object.
+class BaseParser(object):
 
-    :param format: Serialization format (json, yaml, xml, etc)
-    :param stream: file-like obj
+    def __init__(self):
+        # build a format -> media type map
+        self.formats = {
+            renderer.format: renderer.media_type for renderer
+            in api_settings.DEFAULT_RENDERER_CLASSES
+        }
+        # associate media types with their parser classes
+        self.parsers = {
+            parser.media_type: parser for parser
+            in api_settings.DEFAULT_PARSER_CLASSES
+        }
 
-    :raises django.core.serializers.SerializerDoesNotExist:
-        If provided ``format`` value is not registered with a parser class.
+    def deserialize(self, format, stream):
+        """
+        Attempt to read and deserialize data from ``stream``.
+        Return a deserialized object.
 
-    :raises proposal.exceptions.ObjectError: If failed to deserialize data
-        according to format.
-    """
-    # build a format -> media type map
-    format_map = {
-        renderer.format: renderer.media_type for renderer
-        in api_settings.DEFAULT_RENDERER_CLASSES
-    }
-    # assotitate media types with their parser classes
-    parser_map = {
-        parser.media_type: parser for parser
-        in api_settings.DEFAULT_PARSER_CLASSES
-    }
-    try:
-        media_type = format_map[format]
-        parser_class = parser_map[media_type]
-    except KeyError:
-        raise SerializerDoesNotExist(format)
-    else:
-        parser = parser_class()
+        :param format: Serialization format (json, yaml, xml, etc)
+        :param stream: file-like obj
+
+        :raises django.core.serializers.SerializerDoesNotExist:
+            If provided ``format`` value is not registered with a parser class.
+        """
         try:
-            return parser.parse(stream)
-        except:
-            raise ObjectError()
+            media_type = self.formats[format]
+            parser_class = self.parsers[media_type]
+        except KeyError:
+            raise SerializerDoesNotExist(format)
+        else:
+            return parser_class().parse(stream)
 
 
-def videos(path):
+class Videos(BaseParser):
     """
-    Parse categories and videos from a proposal root located at ``path``.
-    Yield ``proposal.serializers.VideoSerializer`` objects.
+    Parser class that takes a file path as an argument.
+
+    An instance of the class is an iterator that walks the file tree
+    seeking for serialized video and category objects, deserializes the objects
+    with either ``proposal.serializers.VideoSerializer`` or
+    ``proposal.serializers.CategorySerializer`` serializers and
+     yields "unsaved" instances of the video serializer class.
 
     :param path: Path to a proposal root (may as well be a pathlib.Path instance)
 
-    :raises proposal.exceptions.TemplateError: If proposal root does not follow
-        the expected file structure.
-
-    :raises proposal.exceptions.ObjectError: If parsed category or video
-        files cannot be deserialized into valid instances of
-        ``proposal.serializers.CategorySerializer`` or
-        ``proposal.serializers.VideoSerializer`` respectively.
+    :raises proposal.exceptions.ProposalError:
+        If proposal root does not follow the correct file structure or contains unserializable files.
     """
-    try:
-        for category_path in force_path(path).iterdir():
-            # skip directories/files that begin with a non-alphanumeric character
-            if not re.match(r'^[a-z0-9]', category_path.stem, flags=re.I):
-                continue
-            # a proposal root must contain directories only
-            if not category_path.is_dir():
-                raise TemplateError('%s is not a directory' % category_path)
+    def __init__(self, path):
+        super(Videos, self).__init__()
+        self.path = path
 
-            video_objects = []
-            category_object = None
+    def __iter__(self):
+        """
+        Return and iterator that walks the file tree seeking for serialized objects
+        and yielding unsaved instances of ``proposal.serializers.VideoSerializer``.
+        """
+        return self._iter_proposal_root(self.path)
 
-            # attempt to deserialize objects using assotiated serializers
-            for video_path in category_path.iterdir():
-                # skip files that begin with a dot
-                if video_path.is_file() and video_path.stem.startswith('.'):
+    def _iter_proposal_root(self, path):
+        # list of caught exceptions
+        exc_list = []
+        try:
+            for category_path in force_path(path).iterdir():
+                # skip directories/files that begin with a non-alphanumeric character
+                if not re.match(r'^[a-z0-9]', category_path.stem, flags=re.I):
                     continue
                 try:
-                    with open(str(video_path), 'rb') as f:
-                        try:
-                            obj = deserialize(video_path.suffix[1:], f)
-                        except SerializerDoesNotExist:
-                            raise TemplateError('failed to deserialize %s' % video_path)
+                    yield from self._iter_category(category_path)
+                except ProposalError as e:
+                    exc_list.append((type(e), str(e)))
+        # catch all uncaught OSError's
+        except OSError as e:
+            raise TemplateError(str(e))
 
-                        if not isinstance(obj, dict):
-                            raise ObjectError('%s does not yield a dictionary' % video_path)
-                except OSError:
-                    raise TemplateError('failed to open %s' % video_path)
+        if exc_list:
+            # provide the caller with a full list of occured errors
+            raise ProposalError(exc_list)
 
-                # this is the category metafile
-                if video_path.stem == settings.PROPOSAL_CATEGORY_META:
-                    # a metafile (probably with another extension) has already been found
-                    if category_object is not None:
-                        raise TemplateError('duplicate category metafiles are found in %s' % category_path)
-                    category_object = obj
-                # this is a video file
-                else:
-                    video_objects.append(obj)
+    def _iter_category(self, path):
+        # a proposal root must contain directories only
+        if not path.is_dir():
+            raise TemplateError('%s is not a directory' % path)
 
-            if category_object is None:
-                raise TemplateError('%s is missing category metafile' % category_path)
+        # list of video objects (dictionaries) found in the directory
+        objects = []
+        # category description object
+        category_obj = None
 
+        # attempt to deserialize objects using associated serializers
+        for video_path in path.iterdir():
+
+            # skip files that begin with a dot
+            if video_path.is_file() and video_path.stem.startswith('.'):
+                continue
+
+            obj = self._get_object(video_path)
+
+            # this is the category metafile
+            if video_path.stem == settings.PROPOSAL_CATEGORY_META:
+                # a metafile (probably with another extension) has already been found
+                if category_obj is not None:
+                    raise TemplateError('duplicate metafiles are found in %s' % path)
+                category_obj = obj
+            # this is a video object
+            else:
+                objects.append(obj)
+
+        # acquire a Category instance either from non-empty description object
+        # or directory name as a category slug
+        category = self._get_category(category_obj, path.stem)
+
+        for video_obj in objects:
+            # assign the saved category id to all assotitated video objects
+            video_obj.update({'category': category.pk})
+            serializer = VideoSerializer(data=video_obj)
+
+            if not serializer.is_valid():
+                raise ObjectError('%s is not a valid video object' % video_obj)
+
+            yield serializer
+
+    def _get_object(self, path):
+        path = force_path(path)
+        try:
+            with open(str(path), 'rb') as f:
+                try:
+                    obj = self.deserialize(path.suffix[1:], f)
+                except SerializerDoesNotExist:
+                    raise TemplateError('%s is not a registered serializer format (%s)' % (path.suffix[1:], path))
+                except Exception as e:
+                    raise ObjectError('failed to deserialize %s (%s)' % (path, e))
+
+                if not isinstance(obj, dict):
+                    raise ObjectError('%s does not yield a dictionary' % path)
+
+                return obj
+        except OSError:
+            raise TemplateError('failed to open %s' % path)
+
+    def _get_category(self, obj, slug=''):
+        if not obj:
+            # attempt to deslugify title
+            title = re.sub(r'\W+', ' ', slug).strip().capitalize()
+
+            # category title must not be empty
+            if not title:
+                raise ProposalError('failed to deslugify "%s"' % slug)
+
+            return Category.objects.get_or_create(slug=slug, defaults={'title': title, 'slug': None})[0]
+        else:
             # attempt to map the deserialized object to a django model
-            category_serializer = CategorySerializer(data=category_object)
+            serializer = CategorySerializer(data=obj)
 
-            if not category_serializer.is_valid():
-                raise ObjectError('%s contains invalid metafile' % category_path)
-            category_serializer.save()
+            if not serializer.is_valid():
+                raise ObjectError('failed to map %s to a model' % obj)
 
-            for video_object in video_objects:
-                # assign the saved category id to all assotitated video objects
-                video_object.update({'category': category_serializer.object.pk})
-                video_serializer = VideoSerializer(data=video_object)
+            return serializer.save() or serializer.object
 
-                if not video_serializer.is_valid():
-                    raise ObjectError('%s is not a valid video object' % video_object)
 
-                yield video_serializer
-
-    # the path is not a directory?
-    except OSError as e:
-        raise TemplateError(str(e))
+# backward compat
+videos = Videos
--- proposal/tests/test_proposal.py
+++ proposal/tests/test_proposal.py
@@ -34,48 +34,44 @@ class TestCase(test.TestCase):
 class InvalidProposalRootTestCase(TestCase):
     test_path = TestCase.test_path / 'invalid_roots'
 
-    def test_category_directory_with_no_files_raises_exception(self):
-        with self.assertRaises(exceptions.TemplateError):
-            self.save_videos(self.test_path / 'no_category_files')
-
     def test_proposal_root_must_contain_directories_only(self):
-        with self.assertRaises(exceptions.TemplateError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'root_contains_files')
 
     def test_category_directory_with_no_metafile_raises_exception(self):
-        with self.assertRaises(exceptions.TemplateError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'no_category_metafile')
 
     def test_category_directory_with_multiple_metafiles_raises_exception(self):
-        with self.assertRaises(exceptions.TemplateError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'multiple_category_metafiles')
 
     def test_category_directory_with_unexpected_video_files_raises_exception(self):
-        with self.assertRaises(exceptions.TemplateError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'unexpected_video_files')
 
     def test_category_directory_with_unknown_file_format_raises_exception(self):
-        with self.assertRaises(exceptions.TemplateError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'unknown_file_format')
 
     def test_improper_yaml_file_raises_exception(self):
-        with self.assertRaises(exceptions.ObjectError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'undeserializable_yaml_files')
 
     def test_improper_json_file_raises_exception(self):
-        with self.assertRaises(exceptions.ObjectError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'undeserializable_json_files')
 
     def test_improper_xml_file_raises_exception(self):
-        with self.assertRaises(exceptions.ObjectError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'undeserializable_xml_files')
 
     def test_category_with_invalid_metafile_object_raises_exception(self):
-        with self.assertRaises(exceptions.ObjectError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'invalid_category_object')
 
     def test_category_with_invalid_video_objects_raises_exception(self):
-        with self.assertRaises(exceptions.ObjectError):
+        with self.assertRaises(exceptions.ProposalError):
             self.save_videos(self.test_path / 'invalid_video_objects')
 
 
@@ -92,6 +88,7 @@ class ValidProposalRootTestCase(TestCase):
 
     def test_namesake_categories_from_different_directories_get_their_videos_merged(self):
         self.save_videos(self.test_path / 'namesake_categories')
+        self.assertEqual(Category.objects.get(title='Foo').videos.count(), 2)
 
     def test_category_video_list_is_not_emptied(self):
         category = Category.objects.create(title='Foo Category')
@@ -110,3 +107,19 @@ class ValidProposalRootTestCase(TestCase):
         # All of the Foo are ignored
         with self.assertRaises(ObjectDoesNotExist):
             Category.objects.get(title='Foo')
+
+    def test_category_is_created_when_parsing_empty_category_directory(self):
+        self.save_videos(self.test_path / 'no_category_files')
+        Category.objects.get(title='Empty category', slug='empty-category')
+
+    def test_category_directory_name_is_looked_up_against_slug_when_no_metafile_provided(self):
+        category = Category.objects.create(title='Test Category')
+
+        self.save_videos(self.test_path / 'no_category_metafile')
+        self.assertEqual(category.videos.count(), 1)
+
+    def test_category_directory_is_not_used_as_source_of_slug_value_for_new_objects(self):
+        self.save_videos(self.test_path / 'uncanonical_slug')
+        # although directory name is looked up against existing slugs
+        # it is ignored when creating new Category objects
+        self.assertEqual(Category.objects.get(title='Test category').slug, 'test-category')
